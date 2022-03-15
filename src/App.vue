ed15d66046c946e711d72fd87107f6bed9172550
miyamaruyuuki <ajptmjg@gmail.com>
2022-03-16 00:06:41 +0900

    編集機能作成中

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
     <h3>ノート一覧</h3>
-    <button id="addButton" v-on:click="addNote">追加</button>
+    <form class="add-form" v-on:submit.prevent="addNote">
+      ノート名 <input type="text" v-model="noteName">
+      <button type="submit">追加</button>
+    </form>
     <tr v-for="(note, index) in notes" :key="index">
-      <td>{{ note.noteName }}</td>
+      <td v-if="!note.isActive">{{ note.noteName }}</td>
+      <input type="text" v-if="note.isActive">
+      <td><button id="updButton" v-show="!note.isActive" v-on:click="updNote(note)">編集</button></td>
+      <td><button id="delButton" v-on:click="delNote(note)">削除</button></td>
     </tr>
   </div>
 </template>
@@ -26,14 +32,35 @@ export default {
           });
     },
     addNote: function () {
-      axios.get("http://127.0.0.1:8000/api/note")
-          .then((res) => {
-            this.notes = res.data;
+      axios.post("http://127.0.0.1:8000/api/note",{
+        noteName: this.noteName
+          }).then(() => {
+            this.getNotes();
           });
+    },
+    updNote: function (note) {
+      note.isActive = true
+      console.log(note)
+      // axios.get('http://127.0.0.1:8000/api/note/' +note.id,{
+      // }).then(()=>{
+      // })
+    },
+    delNote: function (note) {
+      axios.post('http://127.0.0.1:8000/api/note/' +note.id,{
+        _method: 'DELETE'
+      }).then(()=>{
+        this.getNotes();
+      })
     }
   },
   mounted() {
-    this.getNotes();
+    axios.get("http://127.0.0.1:8000/api/note")
+        .then((res) => {
+          this.notes = res.data;
+          this.notes.forEach(function(element){
+            element["isActive"] = false;
+          });
+        });
   }
 }
 </script>
