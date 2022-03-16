d3428732ed8ef214ef8064f0af6a870ad5f7ca6c
miyamaruyuuki <ajptmjg@gmail.com>
2022-03-17 06:48:12 +0900

    再描画がうまくいかない

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
       <button type="submit">追加</button>
     </form>
     <tr v-for="(note, index) in this.notes" :key="index">
-      <td v-if="!note.isActive">{{ note.noteName }}</td>
-      <input type="text" v-if="note.isActive">
+      <td v-if="!note.isActive" key="input-email">{{ note.noteName }}</td>
+      <td v-if="note.isActive" key="input-email">aaa</td>
       <td><button id="updButton" v-show="!note.isActive" v-on:click="updNote(note)">編集</button></td>
       <td><button id="delButton" v-on:click="delNote(note)">削除</button></td>
     </tr>
@@ -20,39 +20,37 @@ import axios from "axios";
 export default {
 data: function () {
 return {
-notes: [],
-noteName: '',
-  isActive:false
+  notes: [],
+  noteName: ''
 }
 },
 methods: {
-getNotes() {
-  axios.get("http://127.0.0.1:8000/api/note")
-      .then((res) => {
-        this.notes = res.data;
-      });
+  getNotes() {
+    axios.get("http://127.0.0.1:8000/api/note")
+        .then((res) => {
+          this.notes = res.data;
+          this.notes.forEach(function (note) {
+            note.isActive = false
+          });
+          console.log(this.notes);
+        });
+  },
+  addNote: function () {
+    axios.post("http://127.0.0.1:8000/api/note", {
+      noteName: this.noteName
+    }).then(() => {
+      //this.getNotes();
+    });
+  },
+  updNote: function (note) {
+    this.$set(note, "isActive", true);
+    console.log(this.notes);
+  },
+  delNote: function () {
+  }
 },
-addNote: function () {
-  axios.post("http://127.0.0.1:8000/api/note",{
-    noteName: this.noteName
-  }).then(() => {
+created() {
     this.getNotes();
-  });
-},
-updNote: function (note) {
-  note.isActive = true
-  console.log(this.notes)
-},
-delNote: function () {
-}
-},
-mounted() {
-  this.getNotes();
-  console.log(this.notes)
-  this.notes.forEach(function(note){
-    this.$set(note, 'isActive', false)
-  });
-  console.log(this.notes)
 }
 }
 </script>
